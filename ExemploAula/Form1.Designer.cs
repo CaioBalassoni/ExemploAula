1979ae8c435c8fb1f1dda9581f849935379ede83
Caio <caio.balassoni1510@gmail.com>
2025-06-11 10:37:18 -0300

    adicionando label usuario

--- ExemploAula/Form1.Designer.cs
+++ ExemploAula/Form1.Designer.cs
@@ -29,23 +29,35 @@
         private void InitializeComponent()
         {
             lblLogin = new Label();
+            lblUsuario = new Label();
             SuspendLayout();
             // 
             // lblLogin
             // 
             lblLogin.AutoSize = true;
             lblLogin.Font = new Font("Comic Sans MS", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblLogin.Location = new Point(343, 36);
+            lblLogin.Location = new Point(337, 32);
             lblLogin.Name = "lblLogin";
             lblLogin.Size = new Size(116, 40);
             lblLogin.TabIndex = 0;
             lblLogin.Text = "LOGIN";
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Font = new Font("Comic Sans MS", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblUsuario.Location = new Point(140, 106);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(133, 40);
+            lblUsuario.TabIndex = 1;
+            lblUsuario.Text = "Usuário:";
             // 
             // frmLogin
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblUsuario);
             Controls.Add(lblLogin);
             Name = "frmLogin";
             Text = "Login";
@@ -56,5 +68,6 @@
         #endregion
 
         private Label lblLogin;
+        private Label lblUsuario;
     }
 }
